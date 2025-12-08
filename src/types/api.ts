ee1f090668844ab6af21f3e4f1b4b3dc979266ff
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2025-12-08 15:34:42 -0300

    feat: tipos

---
+++ src/types/api.ts
@@ -0,0 +1,47 @@
+export type LoginRequestDTO = { email: string; password: string };
+export type AuthResponseDTO = { email: string; name: string; token: string };
+
+
+export type ListingRequestDTO = {
+title: string;
+description: string;
+category: string;
+condition: string;
+city: string;
+state: string;
+images: string[];
+};
+
+
+export type UserSummaryDTO = {
+    id: string;
+    name: string;
+    profilePicture?: string;
+};
+  
+export type ListingResponseDTO = {
+    id: string;
+    title: string;
+    description?: string;
+    category?: string;
+    condition?: string;
+    images?: string[];
+    city?: string;
+    state?: string;
+    status?: string;
+    createdAt?: string;
+    user?: UserSummaryDTO;
+};
+
+
+export interface UserDataRequestDTO {
+    name: string;
+    email: string;
+    password: string;
+    phone: string;
+    city: string;
+    state: string;
+    profilePicture?: string;
+    googleId?: string;
+  }
+  
